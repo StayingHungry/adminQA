7f4b91e44c61ba822b4e38584c1ec74a2e344fab
nanganglei <ganglei_nan@tju.edu.cn>
2022-02-26 10:33:19 +0800

    初始化

---
+++ src/views/charts/echarts.vue
@@ -0,0 +1,216 @@
+<template>
+    <section class="chart-container">
+        <el-row>
+            <el-col :span="12">
+                <div id="chartColumn" style="width:100%; height:400px;"></div>
+            </el-col>
+            <el-col :span="12">
+                <div id="chartBar" style="width:100%; height:400px;"></div>
+            </el-col>
+            <el-col :span="12">
+                <div id="chartLine" style="width:100%; height:400px;"></div>
+            </el-col>
+            <el-col :span="12">
+                <div id="chartPie" style="width:100%; height:400px;"></div>
+            </el-col>
+            <el-col :span="24">
+                <a href="http://echarts.baidu.com/examples.html" target="_blank" style="float: right;">more>></a>
+            </el-col>
+        </el-row>
+    </section>
+</template>
+
+<script>
+    import echarts from 'echarts'
+
+    export default {
+        data() {
+            return {
+                chartColumn: null,
+                chartBar: null,
+                chartLine: null,
+                chartPie: null
+            }
+        },
+
+        methods: {
+            drawColumnChart() {
+                this.chartColumn = echarts.init(document.getElementById('chartColumn'));
+                this.chartColumn.setOption({
+                  title: { text: 'Column Chart' },
+                  tooltip: {},
+                  xAxis: {
+                      data: ["bug", "需求", "迭代", "任务", "流水线", "git"]
+                  },
+                  yAxis: {},
+                  series: [{
+                      name: '销量',
+                      type: 'bar',
+                      data: [5, 20, 36, 10, 10, 20]
+                    }]
+                });
+            },
+            drawBarChart() {
+                this.chartBar = echarts.init(document.getElementById('chartBar'));
+                this.chartBar.setOption({
+                    title: {
+                        text: 'Bar Chart',
+                        subtext: '数据来自网络'
+                    },
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {
+                            type: 'shadow'
+                        }
+                    },
+                    legend: {
+                        data: ['已修复', '修复中']
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: {
+                        type: 'value',
+                        boundaryGap: [0, 0.01]
+                    },
+                    yAxis: {
+                        type: 'category',
+                        data: ['p4', 'p3', 'p2', 'p1', 'p0', 'bug数量']
+                    },
+                    series: [
+                        {
+                            name: '已修复',
+                            type: 'bar',
+                            data: [18203, 23489, 29034, 104970, 131744, 630230]
+                        },
+                        {
+                            name: '修复中',
+                            type: 'bar',
+                            data: [19325, 23438, 31000, 121594, 134141, 681807]
+                        }
+                    ]
+                });
+            },
+            drawLineChart() {
+                this.chartLine = echarts.init(document.getElementById('chartLine'));
+                this.chartLine.setOption({
+                    title: {
+                        text: 'Line Chart'
+                    },
+                    tooltip: {
+                        trigger: 'axis'
+                    },
+                    legend: {
+                        data: ['bug修复1', '开发时效', 'qa时效']
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: {
+                        type: 'category',
+                        boundaryGap: false,
+                        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    series: [
+                        {
+                            name: 'bug修复1',
+                            type: 'line',
+                            stack: '总量',
+                            data: [120, 132, 101, 134, 90, 230, 210]
+                        },
+                        {
+                            name: '开发时效',
+                            type: 'line',
+                            stack: '总量',
+                            data: [220, 182, 191, 234, 290, 330, 310]
+                        },
+                        {
+                            name: 'qa时效',
+                            type: 'line',
+                            stack: '总量',
+                            data: [820, 932, 901, 934, 1290, 1330, 1320]
+                        }
+                    ]
+                });
+            },
+            drawPieChart() {
+                this.chartPie = echarts.init(document.getElementById('chartPie'));
+                this.chartPie.setOption({
+                    title: {
+                        text: 'Pie Chart',
+                        subtext: '纯属虚构',
+                        x: 'center'
+                    },
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        left: 'left',
+                        data: ['用户反馈', '内部反馈', '线上崩溃', '市场反馈', '老板反馈']
+                    },
+                    series: [
+                        {
+                            name: '访问来源',
+                            type: 'pie',
+                            radius: '55%',
+                            center: ['50%', '60%'],
+                            data: [
+                                { value: 335, name: '用户反馈' },
+                                { value: 310, name: '内部反馈' },
+                                { value: 234, name: '线上崩溃' },
+                                { value: 135, name: '市场反馈' },
+                                { value: 1548, name: '老板反馈' }
+                            ],
+                            itemStyle: {
+                                emphasis: {
+                                    shadowBlur: 10,
+                                    shadowOffsetX: 0,
+                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                }
+                            }
+                        }
+                    ]
+                });
+            },
+            drawCharts() {
+                this.drawColumnChart()
+                this.drawBarChart()
+                this.drawLineChart()
+                this.drawPieChart()
+            },
+        },
+
+        mounted: function () {
+            this.drawCharts()
+        },
+        updated: function () {
+            this.drawCharts()
+        }
+    }
+</script>
+
+<style scoped>
+    .chart-container {
+        width: 100%;
+        float: left;
+    }
+    /*.chart div {
+        height: 400px;
+        float: left;
+    }*/
+
+    .el-col {
+        padding: 30px 20px;
+    }
+</style>
